ba74b25d8f1eb44ec1cecc29368b38285729b7ca
mmh <myrfy001@users.noreply.github.com>
2022-07-22 03:15:14 +0000

    os6 basic pass

---
+++ os6/src/syscall/fs.rs
@@ -0,0 +1,120 @@
+//! File and filesystem-related syscalls
+
+use crate::mm::translated_byte_buffer;
+use crate::mm::translated_str;
+use crate::mm::translated_refmut;
+use crate::task::current_user_token;
+use crate::task::current_task;
+use crate::fs::{open_file, hardlink, unlink};
+use crate::fs::OpenFlags;
+use crate::fs::Stat;
+use crate::mm::UserBuffer;
+use alloc::sync::Arc;
+
+pub fn sys_write(fd: usize, buf: *const u8, len: usize) -> isize {
+    let token = current_user_token();
+    let task = current_task().unwrap();
+    let inner = task.inner_exclusive_access();
+    if fd >= inner.fd_table.len() {
+        return -1;
+    }
+    if let Some(file) = &inner.fd_table[fd] {
+        let file = file.clone();
+        // release current task TCB manually to avoid multi-borrow
+        drop(inner);
+        file.write(
+            UserBuffer::new(translated_byte_buffer(token, buf, len))
+        ) as isize
+    } else {
+        -1
+    }
+}
+
+pub fn sys_read(fd: usize, buf: *const u8, len: usize) -> isize {
+    let token = current_user_token();
+    let task = current_task().unwrap();
+    let inner = task.inner_exclusive_access();
+    if fd >= inner.fd_table.len() {
+        return -1;
+    }
+    if let Some(file) = &inner.fd_table[fd] {
+        let file = file.clone();
+        // release current task TCB manually to avoid multi-borrow
+        drop(inner);
+        file.read(
+            UserBuffer::new(translated_byte_buffer(token, buf, len))
+        ) as isize
+    } else {
+        -1
+    }
+}
+
+pub fn sys_open(path: *const u8, flags: u32) -> isize {
+    let task = current_task().unwrap();
+    let token = current_user_token();
+    let path = translated_str(token, path);
+    if let Some(inode) = open_file(
+        path.as_str(),
+        OpenFlags::from_bits(flags).unwrap()
+    ) {
+        let mut inner = task.inner_exclusive_access();
+        let fd = inner.alloc_fd();
+        inner.fd_table[fd] = Some(inode);
+        fd as isize
+    } else {
+        -1
+    }
+}
+
+pub fn sys_close(fd: usize) -> isize {
+    let task = current_task().unwrap();
+    let mut inner = task.inner_exclusive_access();
+    if fd >= inner.fd_table.len() {
+        return -1;
+    }
+    if inner.fd_table[fd].is_none() {
+        return -1;
+    }
+    inner.fd_table[fd].take();
+    0
+}
+
+// YOUR JOB: 扩展 easy-fs 和内核以实现以下三个 syscall
+pub fn sys_fstat(fd: usize, st: *mut Stat) -> isize {
+    let task = current_task().unwrap();
+    let mut inner = task.inner_exclusive_access();
+    if fd >= inner.fd_table.len() {
+        return -1;
+    }
+    if inner.fd_table[fd].is_none() {
+        return -1;
+    }
+    if let Some(t) = inner.fd_table[fd].as_ref().unwrap().fstat() {
+        let token = current_user_token();
+        *translated_refmut(token, st) = t;
+        return 0
+    } else {
+        return -1
+    }
+}
+
+pub fn sys_linkat(old_name: *const u8, new_name: *const u8) -> isize {
+    let token = current_user_token();
+    let old_path = translated_str(token, old_name);
+    let new_path = translated_str(token, new_name);
+    if hardlink(&old_path, &new_path).is_some(){
+        0
+    } else {
+        -1
+    }
+}
+
+pub fn sys_unlinkat(name: *const u8) -> isize {
+    let token = current_user_token();
+    let path = translated_str(token, name);
+    if unlink(&path).is_some(){
+        0
+    } else {
+        -1
+    }
+}
